faa7dfaaee4b07b7c82e9f6edfaca4134bc335e8
0dot77 <yty0706@gmail.com>
2024-09-13 10:01:41 +0900

    0913 study

---
+++ TodoList/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("Hello!");
+Console.WriteLine("What do you want to do?");
+Console.WriteLine("[S]ee all TODOs");
+Console.WriteLine("[A]dd a TODO");
+Console.WriteLine("[R]emove a TODO");
+Console.WriteLine("[E]xit");
+
+var userChoice = Console.ReadLine();
+
+var number = 10;
+var isLargerThan4AndSmallerThan9 = number > 4 && number < 9;
+var isEqualTo2OrLargerThan6 = number == 2 || number > 6;
+
+Console.ReadKey();
+
+
